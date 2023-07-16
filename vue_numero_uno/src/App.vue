157c581451484602c3b554772500268625a5d594
Bruno <77980627+hightallq@users.noreply.github.com>
2023-07-16 12:01:18 +0300

    Add files via upload

---
+++ vue_numero_uno/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { reactive, ref, computed, onMounted } from 'vue'
+import DarkTheme from './components/DarkTheme.vue';
+import RandomMuzhiki from './components/RandomMuzhiki.vue';
+import TodoList from './components/TodoList.vue';
+import TodoListParttwo from './components/TodoListParttwo.vue';
+import Scroll from './components/Scroll.vue';
+
+</script>
+
+<template>
+
+<!-- <RandomMuzhiki /> -->
+<!-- <DarkTheme /> -->
+<!-- <TodoList /> -->
+<!-- <TodoListParttwo /> -->
+<Scroll />
+
+</template>
+
+<style>
+
+</style>

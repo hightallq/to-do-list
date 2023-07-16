268930729bd3aa44c18957e88fd8a95658ac9ade
Bruno <77980627+hightallq@users.noreply.github.com>
2023-07-16 12:02:19 +0300

    Update App.vue

--- vue_numero_uno/src/App.vue
+++ vue_numero_uno/src/App.vue
@@ -1,20 +1,12 @@
 <script setup>
 import { reactive, ref, computed, onMounted } from 'vue'
-import DarkTheme from './components/DarkTheme.vue';
-import RandomMuzhiki from './components/RandomMuzhiki.vue';
-import TodoList from './components/TodoList.vue';
 import TodoListParttwo from './components/TodoListParttwo.vue';
-import Scroll from './components/Scroll.vue';
 
 </script>
 
 <template>
 
-<!-- <RandomMuzhiki /> -->
-<!-- <DarkTheme /> -->
-<!-- <TodoList /> -->
-<!-- <TodoListParttwo /> -->
-<Scroll />
+<TodoListParttwo />
 
 </template>
 
